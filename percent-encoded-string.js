758fefcfd4f4d56a209015bebf9979b0e0b1f6e6
Galip Berk Ozer <galipberkozer@gmail.com>
2019-12-23 23:40:42 +0000

    Function that replaces the whitespace in a string with %20

---
+++ percent-encoded-string.js
@@ -0,0 +1,18 @@
+const urlEncode = function(text) {
+  let trimmedText = text.trim();
+  let encoded = '';
+
+  for (let i = 0; i < trimmedText.length; i++) {
+    if (trimmedText[i] === ' ') {
+      encoded += '%20';
+    } else {
+      encoded += trimmedText[i];
+    }
+  }
+
+  return encoded;
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
