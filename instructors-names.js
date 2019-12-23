29f16801000b9e3a1d0904b5ab33e2e379427ddd
Galip Berk Ozer <galipberkozer@gmail.com>
2019-12-23 23:16:46 +0000

    Function that returns the person with the longest name, given an array of person objects

---
+++ instructors-names.js
@@ -0,0 +1,23 @@
+const instructorWithLongestName = function(instructors) {
+  let longestName = instructors[0];
+
+  for (let person of instructors) {
+    if (person.name.length > longestName.name.length) {
+      longestName = person;
+    } 
+  }
+
+  return longestName;
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
