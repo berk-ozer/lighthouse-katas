246b0caa8b35444f6bbe31c0a573696c3ad48b63
Galip Berk Ozer <galipberkozer@gmail.com>
2020-01-20 20:32:46 +0000

    Function that repeats numbers as per the inputted two-dimensional array

---
+++ repeating-numbers.js
@@ -0,0 +1,19 @@
+const repeatNumbers = function(arr) {
+  let output = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let line = '';
+    
+    for (let j = 0; j < arr[i][1]; j++) {
+      line += arr[i][0];
+    }
+
+    output.push(line);
+  }
+  
+  return output.join(', ');
+}
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
